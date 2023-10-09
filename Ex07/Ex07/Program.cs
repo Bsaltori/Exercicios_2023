386da565f3c676685b937c02f96e87f583505fb5
Bsaltori <biasaltori@yahoo.com.br>
2023-10-09 09:35:58 -0300

    exercicios de laços de repetição

---
+++ Ex07/Ex07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Write a program that prints a multiplication table for numbers up to 12.
+
+int result, i;
+
+
+
+
+for (i = 1; i <= 12; i++)
+{
+    result = 12 * i;
+    Console.WriteLine($"12 x {i} = {result}");
+}
+
+
---
+++ Ex08/Ex08/Program.cs
@@ -0,0 +1,31 @@
+﻿// Write a program that prints all prime numbers. (Note: if your programming language does not support
+// arbitrary size numbers, printing all primes up to the largest number you can easily represent is fine too.)
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Console.Write("Escreva um número: ");
+        int n = 2;
+        while (true)
+        {
+            int count = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 1)
+            {
+                Console.WriteLine("Numero " + n + " e primo.");
+            }
+            n++;
+        }
+    }
+}
